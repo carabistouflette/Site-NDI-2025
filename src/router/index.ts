8c102ee64d9c9f4c98e1492b5bada1ad1522558a
Théodore de Boisseson <theodore.de-boisseson@etu.umontpellier.fr>
2025-12-05 06:51:21 +0100

    prepare pour merge

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import MenuView from '../views/MenuView.vue'
 import ChallengeView from '../views/ChallengeView.vue'
+import ChallengeLayout from '../layouts/ChallengeLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,13 +14,19 @@ const router = createRouter({
     },
     {
       path: '/challenge',
-      name: 'menu',
-      component: MenuView
-    },
-    {
-      path: '/challenge/:id',
-      name: 'challenge',
-      component: ChallengeView
+      component: ChallengeLayout,
+      children: [
+        {
+          path: '',
+          name: 'menu',
+          component: MenuView
+        },
+        {
+          path: ':id',
+          name: 'challenge',
+          component: ChallengeView
+        }
+      ]
     }
   ]
 })
