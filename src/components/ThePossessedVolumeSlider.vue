b2630308b416b08b6bb53fec40dc68b7bc620a95
Alexis ROBIN <nek0@tutanota.com>
2025-12-05 06:22:21 +0100

    Add placeholder components for Volume Game to fix build

---
+++ src/components/ThePossessedVolumeSlider.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="p-8 text-center border border-red-500 rounded">
+    <h2 class="text-xl text-red-500 mb-4">SLIDER POSSÉDÉ (WIP)</h2>
+    <input type="range" class="w-full" />
+  </div>
+</template>
